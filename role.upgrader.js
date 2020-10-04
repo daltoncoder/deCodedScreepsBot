8b9fa54a611e9e6fe3097051a27c93235dcb2739
daltoncoder <daltoncoder2@gmail.com>
2020-10-04 17:38:29 -0400

    Update role.upgrader.js

--- role.upgrader.js
+++ role.upgrader.js
@@ -20,23 +20,36 @@ var roleUpgrader = {
         else {
             var sources = creep.pos.findClosestByPath(FIND_STRUCTURES, {filter: (structure) => {return ((structure.structureType == STRUCTURE_STORAGE || 
             structure.structureType == STRUCTURE_CONTAINER) && structure.store[RESOURCE_ENERGY] > 50)}});
-            console.log(creep + " " + sources);
             if (sources == null){
                 var ruin = creep.pos.findClosestByPath(FIND_RUINS, {
                     filter: (ruin) => { return ruin.store[RESOURCE_ENERGY] > 0;
                 }});
-
+                if(ruin){
+                var groundTarget = null;
                 var target = ruin;
+                }
+                else{
+                    var droppedEnergy =creep.room.find(FIND_DROPPED_RESOURCES, {filter: (resource) => { return resource.resourceType == RESOURCE_ENERGY}});
+                    if(droppedEnergy.length > 0){
+                        var target = null;
+                        var groundTarget = droppedEnergy[0];
+                    }
+                }
             }
             else{
-                console.log(sources + '1');
+                var groundTarget = null;
                 var target = sources;
-
-                console.log('creep:' + creep + 'target: ' + target)
             }
+            if(target){
             if(creep.withdraw(target,RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(target, {visualizePathStyle: {stroke: '#ffaa00'}});
             }
+        }
+            else if(groundTarget) [
+                if(creep.pickup(groundTarget) == ERR_NOT_IN_RANGE){
+                    creep.moveTo(groundTarget);
+                }
+            ]      
         }
     }
 };
