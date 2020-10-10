271adcb2a51d65c4b79f9f242e391ee6f8618612
daltoncoder <daltoncoder2@gmail.com>
2020-10-09 20:07:38 -0400

    Update role.signbitch.js

--- role.signbitch.js
+++ role.signbitch.js
@@ -1,5 +1,5 @@
 var run = function(creep){
-    if(!room.controller.sign || creep.room.controller.sign.username != 'Mcguiver') {
+    if(!creep.room.controller.sign || creep.room.controller.sign.username != 'Mcguiver') {
         if(creep.signController(creep.room.controller, "New programmer and screeps player trying to write my first screeps bot. Very friendly player. Please message me if you have any advice or notice anything off about my code. Also if my code is interfering with what your trying to do just shoot me a message. Thanks and goodluck!") == ERR_NOT_IN_RANGE) {
             creep.moveTo(creep.room.controller);
         }
