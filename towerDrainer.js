5b70d9b68c29caca00438dbc26a142bd76fa9b69
daltoncoder <71679972+daltoncoder@users.noreply.github.com>
2020-10-16 18:00:25 -0400

    Update towerDrainer.js

--- towerDrainer.js
+++ towerDrainer.js
@@ -34,7 +34,31 @@ var towerDrainer = function (room) {
   if(enemyTower.store[RESOURCE_ENERGY] == 0){
     tanker.memory.roomClear = true;
   }
+  else{
+    tanker.memory.roomClear = false;
+  }
 }
+if(tanker.memory.roomClear == true);{
+  Game.spawns['Spawn2'].spawnCreep([TOUGH,TOUGH,MOVE,MOVE,MOVE,MOVE,ATTACK,ATTACK], 'attacker', {memory:{role: 'attacker'}});
+}
+if(Game.creeps['attacker']){
+  var attacker = Game.creeps[attacker];
+  var nextToTower= new RoomPosition(34,14, 'E23N23');
+  if(!attacker.memory.inRoom){
+    attacker.moveTo(nextToTower);
+  }
+  if(attacker.pos.isEqualTo(nextToTower)){
+    attacker.memory.inRoom = true;
+    var enemySpawn = Game.getObjectById('5f88388b38d6233ae6d352d9');
+    if(enemyTower){
+    attacker.attack(enemyTower);
+  }
+  else if(enemySpawn){
+    attacker.attack(enemySpawn);
+  }
+  }
+}
+
 
 
 };
