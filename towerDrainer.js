a79e6aa2561d40ea908a707ab43bc2a327f91cb9
daltoncoder <71679972+daltoncoder@users.noreply.github.com>
2020-10-16 18:18:20 -0400

    Update towerDrainer.js

--- towerDrainer.js
+++ towerDrainer.js
@@ -39,7 +39,7 @@ var towerDrainer = function (room) {
   }
 }
 if(tanker.memory.roomClear == true);{
-  Game.spawns['Spawn2'].spawnCreep([TOUGH,TOUGH,MOVE,MOVE,MOVE,MOVE,ATTACK,ATTACK], 'attacker', {memory:{role: 'attacker'}});
+  Game.spawns['Spawn2'].spawnCreep([ATTACK,ATTACK,ATTACK,ATTACK,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,ATTACK,ATTACK], 'attacker', {memory:{role: 'attacker'}});
 }
 if(Game.creeps['attacker']){
   var attacker = Game.creeps['attacker'];
